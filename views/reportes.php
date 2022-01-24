7012d8eb82807d5e9791cc3cc5cd99522310545b
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2022-01-24 11:25:21 -0600

    Update reportes.php

--- views/reportes.php
+++ views/reportes.php
@@ -52,12 +52,12 @@
       <!-- ----------------------------  FORMULARIO 1 Tabs  ---------------------------------------->
         <div  id="test-swipe-1" class="col s12">
           <div class="row">
-            <div class="col s12 l5 m5">
+            <div class="col s10 l5 m5">
                 <label for="fecha">DIA:</label>
                 <input id="fecha" type="date" >
             </div>
             <br>
-            <div class="col s2 l3 m3 right">
+            <div class="col s10 l3 m3 right">
               <button class="btn waves-light waves-effect indigo darken-1" onclick="buscar_pagos(1);"><i class="material-icons prefix right">send</i>BUSCAR</button>
             </div>           
           </div>
